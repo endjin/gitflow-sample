fe6d6773f00d5215c35a39677d2af0d547c0c63a
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2012-10-15 22:17:23 +0100

    Add default build and solution

---
+++ Solutions/Sample.ServiceHost/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Sample.ServiceHost
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
